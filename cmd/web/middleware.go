30cf9de88b05a10d6bbb389461011611744ce4ec
JLL32 <eabduljalil@outlook.com>
2023-06-21 20:23:19 +0100

    refactor routes with httprouter and clean URLs

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -32,15 +32,14 @@ func (app *application) logRequests(next http.Handler) http.Handler {
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    defer func() {
-      if err := recover(); err != nil {
-        w.Header().Set("Connection", "close")
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
 
-        app.serverError(w, fmt.Errorf("%s", err))
-      }
-    }()
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
 
 		next.ServeHTTP(w, r)
 	})
 }
-
