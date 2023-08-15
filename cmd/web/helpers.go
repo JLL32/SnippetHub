00d95afdc5c4a6173ec35a076a50685d4e8f19b3
JLL32 <eabduljalil@outlook.com>
2023-08-15 16:28:53 +0100

    add authorization and nosurf for protection against CSRF

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -51,8 +52,10 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
@@ -73,4 +76,10 @@ func (app *application) decodePostFrom(r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	val := app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	fmt.Printf("is authenticated: %v", val)
+	return val
 }
